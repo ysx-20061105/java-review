5243da1f52f04ac2bd89d322e5c83170ee09ac1a
20061105 <228343836@qq.com>
2022-12-30 22:26:07 +0800

    修改文件名

---
+++ README.md
@@ -0,0 +1,11 @@
+## java review
+java基础复习
+
+包含：
+1. io
+2. file
+3. exception
+4. jdbc
+5. collection
+6. net
+7. thread
---
+++ src/exception/Exception.java
@@ -0,0 +1,26 @@
+package exception;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+public class Exception {
+    @Test
+    public void test0(){
+        //ArrayIndexOutOfBoundsException
+        int[] arr=new int[3];
+        System.out.println(arr[3]);
+    }
+    @Test
+    public void test01(){
+        //NullPointerException
+        String str=null;
+        System.out.println(str.charAt(0));
+    }
+    @Test
+    public void test02(){
+        //ClassCastException
+        Object obj=new Date();
+        String str=(String) obj;
+    }
+}
