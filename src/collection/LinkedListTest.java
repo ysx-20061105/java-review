d540722849b0fb7905985481cf3dcd1136a5bd10
20061105 <228343836@qq.com>
2022-12-31 15:34:08 +0800

    Set集合

---
+++ src/collection/LinkedListTest.java
@@ -0,0 +1,19 @@
+package collection;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+    @Test
+    public void test(){
+        LinkedList linkedList = new LinkedList();
+        /*
+            定义一个私有内部类 Node{e,next,prev} 实现节点
+            next指向下一个节点，prev指向前一个节点
+            add()方法
+
+         */
+    }
+}
